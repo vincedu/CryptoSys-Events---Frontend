8d2b20916a0ee36f3b122511c0498b6a02a79aee
Marc-André Jolicoeur <marc-andre.jolicoeur@polymtl.ca>
2020-11-25 12:51:31 -0500

    fix event dashboard UI

--- src/scenes/Dashboard/Dashboard.jsx
+++ src/scenes/Dashboard/Dashboard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { PageContainer } from '@components';
+import { PageContainer, TitledPaper } from '@components';
 import CreatedEventList from './CreatedEventList/CreatedEventList';
 import GlobalStats from './GlobalStats/GlobalStats';
 
 const Dashboard = () => {
     const { t } = useTranslation();
     return (
-        <PageContainer title={t('eventsDashboard.title')}>
-            <GlobalStats />
-            <CreatedEventList />
+        <PageContainer>
+            <TitledPaper title={t('eventsDashboard.title')}>
+                <GlobalStats />
+                <CreatedEventList />
+            </TitledPaper>
         </PageContainer>
     );
 };
