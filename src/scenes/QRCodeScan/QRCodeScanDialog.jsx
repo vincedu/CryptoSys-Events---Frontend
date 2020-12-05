9621b6a7c82a25d29ad9d924f68aa7b34c02446f
Marc-André Jolicoeur <marc-andre.jolicoeur@polymtl.ca>
2020-12-04 23:24:08 -0500

    show event item after scaning

--- src/scenes/QRCodeScan/QRCodeScanDialog.jsx
+++ src/scenes/QRCodeScan/QRCodeScanDialog.jsx
@@ -4,15 +4,33 @@ import { Dialog } from '@material-ui/core';
 import QrReader from 'react-qr-reader';
 import { TitledPaper, PageContainer } from '@components';
 import { useTranslation } from 'react-i18next';
+import { EVENT_BY_ID_QUERY } from '@graphql/queries';
+import { useQuery } from '@apollo/client';
+import CustomEventItem from '../SearchPage/components/CustomEventItem';
 
 const QRCodeScanDialog = (props) => {
     const { t } = useTranslation();
-    const [result, setresult] = useState(t('scanTicket.noResult'));
+    const [result, setresult] = useState(null);
     const { open, onClose } = props;
+
+    const query = useQuery(EVENT_BY_ID_QUERY, {
+        variables: { id: null },
+    });
+
+    const fetchEvent = (id) => {
+        query
+            .fetchMore({
+                variables: { id },
+            })
+            .then((event) => {
+                if (event.data.eventById) setresult(event.data.eventById);
+                else setresult(t('scanTicket.noResult'));
+            });
+    };
 
     const handleScan = (data) => {
         if (data) {
-            setresult(data);
+            fetchEvent(JSON.parse(data).event.id);
         }
     };
 
@@ -29,9 +47,20 @@ const QRCodeScanDialog = (props) => {
             <PageContainer>
                 <TitledPaper title={t('scanTicket.holdSteady')}>
                     <QrReader delay={300} onError={handleError} onScan={handleScan} style={{ width: '100%' }} />
-                    <p>
-                        {t('scanTicket.scanResult')}: {result}
-                    </p>
+                    {result?.id ? (
+                        <div style={{ paddingTop: 25 }}>
+                            <CustomEventItem
+                                id={result.id}
+                                name={result.name}
+                                description={result.description}
+                                date={result.startDate}
+                                image={result.image}
+                                type={result.type}
+                                languages={result.languages}
+                                tags={result.tags}
+                            />
+                        </div>
+                    ) : null}
                 </TitledPaper>
             </PageContainer>
         </Dialog>
